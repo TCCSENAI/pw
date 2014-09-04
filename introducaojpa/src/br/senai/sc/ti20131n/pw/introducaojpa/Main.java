cef6e1851915cd389b31266f6af461ccc6b15eca
TCCSENAI <aldairjvieira@hotmail.com>
2014-09-04 00:32:08 -0300

    Inserir no banco

--- introducaojpa/src/br/senai/sc/ti20131n/pw/introducaojpa/Main.java
+++ introducaojpa/src/br/senai/sc/ti20131n/pw/introducaojpa/Main.java
@@ -1,15 +1,44 @@
 package br.senai.sc.ti20131n.pw.introducaojpa;
 
+
+import java.util.Scanner;
+
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+
+import br.senai.sc.ti20131n.pw.introducaojpa.entity.Banda;
+
+
+
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("introducaojpa_pu");
-		
-		entityManagerFactory.close();
+		// TODO Auto-generated method stub
+      EntityManagerFactory EntityManagerFactory = Persistence.createEntityManagerFactory("introducaojpa_pu");
+      
+      EntityManager entityManager = EntityManagerFactory.createEntityManager();
+      
+      //entityManager.getTransaction().begin();
+      Scanner Scanner = new Scanner (System.in);
+     Banda banda;
+     for (int i = 0; i < 2; i++) {
+    	 banda = new Banda();
+	     System.out.println("nome da banda");
+	     
+	     banda.setNome(Scanner.nextLine());
+	     entityManager.getTransaction().begin();
+		 entityManager.persist(banda);
+		 entityManager.getTransaction().commit();      
+	 
+     }
+      
 
-	}
-
+      
+     entityManager.close();
+         EntityManagerFactory.close();
+         
+}
 }
